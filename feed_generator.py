30f85998107ea9e8735694ba52b4e1957ad61bfc
Stephaniegraceh <158759339+Stephaniegraceh@users.noreply.github.com>
2024-02-19 19:47:27 +0000

    Update feed_generator.py

--- feed_generator.py
+++ feed_generator.py
@@ -9,17 +9,13 @@ Original file is located at
 
 # feed_generator.py
 
-import feedparser
-import re
-from feedgen.feed import FeedGenerator
-
 def fetch_filtered_articles(rss_feeds, keywords):
     filtered_articles = []
     for feed_url in rss_feeds:
         feed = feedparser.parse(feed_url)
         for entry in feed.entries:
-            # Check directly if 'summary' is in entry
-            entry_summary = entry['summary'] if 'summary' in entry else ''
+            # Safely access 'summary' by checking its existence first
+            entry_summary = entry.summary if hasattr(entry, 'summary') else ''
             content = entry.title + ' ' + entry_summary
             if any(re.search(re.escape(keyword), content, re.IGNORECASE) for keyword in keywords):
                 article = {
@@ -29,6 +25,7 @@ def fetch_filtered_articles(rss_feeds, keywords):
                 }
                 filtered_articles.append(article)
     return filtered_articles
+
 
 def generate_rss_feed(filtered_articles):
     fg = FeedGenerator()
